fed84efa32200941502408d6bf0ba391062aae07
Najmul Huda <88929620+Beast9344@users.noreply.github.com>
2025-07-05 13:22:00 +0600

    update

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ const Logo = ({ className }: { className?: string }) => {
   return (
     <div className={cn('flex items-center', className)}>
       <Image
-        src="https://placehold.co/150x40.png"
+        src="/public/logo.png"
         alt="Immutag Logo"
         width={150}
         height={40}
